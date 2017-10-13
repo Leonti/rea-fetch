464edd161dfe2ffe1699778cf3a22520c890454f
Leonti Bielski <prishelec@gmail.com>
2017-10-14 00:38:16 +1100

    Use specific version of haskell-static-build

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 version=$(date +"%y.%m.%d.%H.%M")
 
-sudo docker run -v "$(pwd):/src" --rm leonti/haskell-static-build
+sudo docker run -v "$(pwd):/src" --rm leonti/haskell-static-build:17.09.28.00.34
 
 sudo docker build -t leonti/rea-fetch:$version .
 sudo docker push leonti/rea-fetch:$version
