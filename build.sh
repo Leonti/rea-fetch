26f0ddcfc765fd21137dcd1ee7627e2acf4e0b65
Leonti Bielski <prishelec@gmail.com>
2017-10-14 01:42:51 +1100

    Mount stack home

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 version=$(date +"%y.%m.%d.%H.%M")
 
-sudo docker run -v "$(pwd):/src" --rm leonti/haskell-static-build:17.09.28.00.34
+sudo docker run -v "$(pwd):/src" -v /tmp/stack-rea-fetch:/root/.stack --rm leonti/haskell-static-build:17.09.28.00.34
 
 sudo docker build -t leonti/rea-fetch:$version .
 sudo docker push leonti/rea-fetch:$version
